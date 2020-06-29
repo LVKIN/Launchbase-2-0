035f6d46051f80a094f841b0ece9a500adff2287
LVKIN <backuplucas6150@gmail.com>
2020-06-29 15:31:30 -0300

    bugs fixes

--- challenges/data.js
+++ challenges/data.js
@@ -1,15 +1,21 @@
 module.exports = [
   {
+    id: "starter",
+    img: "64c237ccff807c054339a62d53b4b402",
     title: "Starter",
     module: "2 Phases",
     price: "Free"
   },
   {
+    id: "launchbase",
+    img: "0828532024cb46921a6b5e941f8d788d",
     title: "LaunchBase",
     module: "8 Weeks",
     price: "Paid"
   },
   {
+    id: "gostack",
+    img: "83a178a0653dab1d55e2ed7946465975",
     title: "Go Stack!",
     module: "8 weeks",
     price: "Paid"
