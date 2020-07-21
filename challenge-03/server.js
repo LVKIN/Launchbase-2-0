177f552b3488ff2dd2ceed022d6c5adf76d07e58
LVKIN <backuplucas6150@gmail.com>
2020-07-21 18:47:02 -0300

    starting at third module and new challenges

---
+++ challenge-03/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+  express: server,
+  autoescape: false,
+  noCache: true
+})
+
+server.get("/", function(req, res){
+  return res.render("teachers")
+})
+
+server.listen(5005, function(){
+  console.log("server is running");
+})
---
+++ gym-management/public/scripts.js
@@ -0,0 +1,8 @@
+const cards = document.querySelectorAll('.card')
+
+for(let card of cards){
+  card.addEventListener("click", function(){
+    const videoID = card.getAttribute("id")
+    window.location.href = `/videos/preview?id=${videoID}`
+  })
+}
